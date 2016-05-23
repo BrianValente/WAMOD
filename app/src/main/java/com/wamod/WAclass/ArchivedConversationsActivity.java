ededae6e6e32e7060d05d1ee21f8999eb4449034
Brian Valente <briannvalente@gmail.com>
2016-05-22 23:12:42 -0300

    Night mode for ArchivedConversationsActivity

--- app/src/main/java/com/wamod/WAclass/ArchivedConversationsActivity.java
+++ app/src/main/java/com/wamod/WAclass/ArchivedConversationsActivity.java
@@ -2,8 +2,11 @@ package com.wamod.WAclass;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.support.v4.widget.ListViewAutoScrollHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.FrameLayout;
+import android.widget.ListView;
 
 import com.wamod.R;
 import com.wamod.Utils;
@@ -11,20 +14,11 @@ import com.wamod.Utils;
 /**
  * Created by brianvalente on 9/20/15.
  */
-public class ArchivedConversationsActivity extends AppCompatActivity {
-    @Override
-    public void onCreate(Bundle b) {
-        super.onCreate(b);
-
-        initDarkMode(this);
-        Utils.loadColors(this);
-        Utils.setTaskDescription(this);
-    }
-
-    public static void initDarkMode(Activity activity) {
+public class ArchivedConversationsActivity {
+    public static void _onCreate(AppCompatActivity a) {
         if (Utils.nightModeShouldRun()) {
-            FrameLayout container = (FrameLayout) activity.findViewById(R.id.action0);
-            container.setBackgroundColor(Utils.getDarkColor(2));
+            ListView list = (ListView) a.findViewById(android.R.id.list);
+            list.setBackgroundColor(Utils.getDarkColor(2));
         }
     }
 }
